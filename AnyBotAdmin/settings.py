984472187b6146bdbb56c5fdec163b1076f5c07b
perevozchikov <41132287+perevozchikov@users.noreply.github.com>
2018-07-30 15:49:04 +1000

    Add files via upload

--- AnyBotAdmin/settings.py
+++ AnyBotAdmin/settings.py
@@ -18,11 +18,10 @@ import os
 #env = environ.Env(DEBUG=(bool, False))
 #environ.Env.read_env()
 
-BASE_DIR = str(root)
 #SECRET_KEY = env.str('SECRET_KEY', 'my-santa-claus')
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
