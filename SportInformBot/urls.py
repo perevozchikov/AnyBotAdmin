0067df40908506e48747b8c40fc2dc8770a7acb9
perevozchikov <41132287+perevozchikov@users.noreply.github.com>
2018-07-30 16:44:21 +1000

    Add files via upload

--- SportInformBot/urls.py
+++ SportInformBot/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from .views import CommandReceiveView
 
 urlpatterns = [
-    url(r'^bot/(?P<631895922:AAHrRowaIaY3OHB6KN6hw9fwKt02NMtz-LI>.+)/$', CommandReceiveView.as_view(), name='command'),
+    url(r'^bot/(?P<bot_token>.+)/$', CommandReceiveView.as_view(), name='command'),
 ]
