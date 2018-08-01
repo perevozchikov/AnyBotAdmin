44ba5890b8c6f419077df6ddae66029e8d8636af
perevozchikov <41132287+perevozchikov@users.noreply.github.com>
2018-08-01 17:12:22 +1000

    Add files via upload

--- SportInformBot/urls.py
+++ SportInformBot/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from .views import command_start
 
 urlpatterns = [
-    url(r'^bot/(?P<bot_token>.+)/$', command_start.as_view(), name='command'),
+    url(r'^bot/(?P<bot_token>.+)/$', command_start, name='command'),
 ]
