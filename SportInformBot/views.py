021ae449dbabb0f7c38d498abfdc2b090d9acd4c
perevozchikov <41132287+perevozchikov@users.noreply.github.com>
2018-08-03 14:59:57 +1000

    Add files via upload

--- SportInformBot/views.py
+++ SportInformBot/views.py
@@ -4,6 +4,8 @@ import json
 import logging
 
 import telepot
+from telepot.namedtuple import LabeledPrice, ShippingOption
+from telepot.delegate import per_invoice_payload, pave_event_space, create_open, per_message, call
 from django.template.loader import render_to_string
 from django.http import HttpResponseForbidden, HttpResponseBadRequest, JsonResponse
 from django.views.generic import View
@@ -29,6 +31,84 @@ def _display_football_feed():
 def _display_hockey_feed():
     return render_to_string('feed.md', {'items': parse_hockey_sportru_rss()})
 
+def _start_payments():
+    TOKEN = settings.TELEGRAM_BOT_TOKEN
+    bot = telepot.DelegatorBot(TOKEN, [
+        (per_message(flavors=['chat']), call(send_invoice)),
+        pave_event_space()(per_invoice_payload(), create_open, OrderProcessor, timeout=30,)
+        ])
+    msg = seed_tuple[1]
+
+    content_type, chat_type, chat_id = telepot.glance(msg)
+
+    if content_type == 'text':
+        bot.sendInvoice(chat_id, "Nick's Hand Cream", "Keep a man's hand like a woman's",
+            payload='a-string-identifying-related-payment-messages-tuvwxyz',
+            provider_token=settings.PAYMENT_PROVIDER_TOKEN,
+            start_parameter='abc',
+            currency='HKD', prices=[
+                LabeledPrice(label='One Case', amount=987),
+                LabeledPrice(label='Package', amount=12)],
+            need_shipping_address=True, is_flexible=True)  # required for shipping query
+
+class OrderProcessor(telepot.helper.InvoiceHandler):
+    def __init__(self, *args, **kwargs):
+        super(OrderProcessor, self).__init__(*args, **kwargs)
+
+    def on_shipping_query(self, msg):
+        query_id, from_id, invoice_payload = telepot.glance(msg, flavor='shipping_query')
+
+        #print('Shipping query:')
+        #pprint(msg)
+
+        bot.answerShippingQuery(
+            query_id, True,
+            shipping_options=[
+                ShippingOption(id='fedex', title='FedEx', prices=[
+                    LabeledPrice(label='Local', amount=345),
+                    LabeledPrice(label='International', amount=2345)]),
+                ShippingOption(id='dhl', title='DHL', prices=[
+                    LabeledPrice(label='Local', amount=342),
+                    LabeledPrice(label='International', amount=1234)])])
+
+    def on_pre_checkout_query(self, msg):
+        query_id, from_id, invoice_payload = telepot.glance(msg, flavor='pre_checkout_query')
+
+        #print('Pre-Checkout query:')
+        #pprint(msg)
+
+        bot.answerPreCheckoutQuery(query_id, True)
+
+    def on_chat_message(self, msg):
+        content_type, chat_type, chat_id = telepot.glance(msg)
+
+        if content_type == 'successful_payment':
+            #print('Successful payment RECEIVED!!!')
+            #pprint(msg)
+            pass
+        else:
+            #print('Chat message:')
+            #pprint(msg)
+            pass
+
+#def send_invoice(seed_tuple):
+#    msg = seed_tuple[1]
+
+#    content_type, chat_type, chat_id = telepot.glance(msg)
+
+#    if content_type == 'text':
+#        bot.sendInvoice(chat_id, "Nick's Hand Cream", "Keep a man's hand like a woman's",
+#            payload='a-string-identifying-related-payment-messages-tuvwxyz',
+#            provider_token=PAYMENT_PROVIDER_TOKEN,
+#            start_parameter='abc',
+#            currency='HKD', prices=[
+#                LabeledPrice(label='One Case', amount=987),
+#                LabeledPrice(label='Package', amount=12)],
+#            need_shipping_address=True, is_flexible=True)  # required for shipping query
+
+        #print('Invoice sent:')
+        #pprint(sent)
+
 class CommandReceiveView(View):
     def post(self, request, bot_token):
         if bot_token != settings.TELEGRAM_BOT_TOKEN:
@@ -39,6 +119,7 @@ class CommandReceiveView(View):
             'help': _display_help,
             'football_feed': _display_football_feed,
             'hockey_feed': _display_hockey_feed,
+            'buy': _start_payments,
         }
 
         raw = request.body.decode('utf-8')
