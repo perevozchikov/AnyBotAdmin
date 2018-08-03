11d993b4a58e3d8cfa6994ce8413a021eb0bc535
perevozchikov <41132287+perevozchikov@users.noreply.github.com>
2018-08-03 15:22:20 +1000

    Add files via upload

--- SportInformBot/views.py
+++ SportInformBot/views.py
@@ -4,8 +4,6 @@ import json
 import logging
 
 import telepot
-from telepot.namedtuple import LabeledPrice, ShippingOption
-from telepot.delegate import per_invoice_payload, pave_event_space, create_open, per_message, call
 from django.template.loader import render_to_string
 from django.http import HttpResponseForbidden, HttpResponseBadRequest, JsonResponse
 from django.views.generic import View
@@ -45,57 +43,6 @@ def _display_football_feed():
 
 def _display_hockey_feed():
     return render_to_string('feed.md', {'items': parse_hockey_sportru_rss()})
-
-def _start_payments():
-    TOKEN = settings.TELEGRAM_BOT_TOKEN
-    bot = telepot.DelegatorBot(TOKEN, [
-        (per_message(flavors=['chat']), call(send_invoice)),
-        pave_event_space()(per_invoice_payload(), create_open, OrderProcessor, timeout=30,)
-        ])
-
-class OrderProcessor(telepot.helper.InvoiceHandler):
-    def __init__(self, *args, **kwargs):
-        super(OrderProcessor, self).__init__(*args, **kwargs)
-
-    def on_shipping_query(self, msg):
-        query_id, from_id, invoice_payload = telepot.glance(msg, flavor='shipping_query')
-
-        #print('Shipping query:')
-        #pprint(msg)
-
-        bot.answerShippingQuery(
-            query_id, True,
-            shipping_options=[
-                ShippingOption(id='fedex', title='FedEx', prices=[
-                    LabeledPrice(label='Local', amount=345),
-                    LabeledPrice(label='International', amount=2345)]),
-                ShippingOption(id='dhl', title='DHL', prices=[
-                    LabeledPrice(label='Local', amount=342),
-                    LabeledPrice(label='International', amount=1234)])])
-
-    def on_pre_checkout_query(self, msg):
-        query_id, from_id, invoice_payload = telepot.glance(msg, flavor='pre_checkout_query')
-
-        #print('Pre-Checkout query:')
-        #pprint(msg)
-
-        bot.answerPreCheckoutQuery(query_id, True)
-
-    def on_chat_message(self, msg):
-        content_type, chat_type, chat_id = telepot.glance(msg)
-
-        if content_type == 'successful_payment':
-            #print('Successful payment RECEIVED!!!')
-            #pprint(msg)
-            pass
-        else:
-            #print('Chat message:')
-            #pprint(msg)
-            pass
-
-
-        #print('Invoice sent:')
-        #pprint(sent)
 
 class CommandReceiveView(View):
     def post(self, request, bot_token):
