6f67854528007610416cae3c4870c5d17c77455c
perevozchikov <41132287+perevozchikov@users.noreply.github.com>
2018-08-06 21:57:30 +1000

    Update views.py

--- SportInformBot/views.py
+++ SportInformBot/views.py
@@ -27,7 +27,7 @@ def _display_football_feed(chat_id):
     football_items = parse_football_sportru_rss()
    # for news in football_items:
    #     fmsg = render_to_string('feed.md', {'items': news})
-    TelegramBot.sendMessage(chat_id, str(type(football_feed)), parse_mode='Markdown')
+    TelegramBot.sendMessage(chat_id, str(type(football_items)), parse_mode='Markdown')
 
     return None
 
