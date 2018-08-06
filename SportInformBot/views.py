6623aae19fed329c83db7fff8050fd60c2c32476
perevozchikov <41132287+perevozchikov@users.noreply.github.com>
2018-08-06 21:29:21 +1000

    Add files via upload

--- SportInformBot/views.py
+++ SportInformBot/views.py
@@ -19,11 +19,11 @@ TelegramBot = telepot.Bot(settings.TELEGRAM_BOT_TOKEN)
 logger = logging.getLogger('telegram.bot')
 
 
-def _display_help():
+def _display_help(chat_id):
     TelegramBot.sendMessage(chat_id, render_to_string('help.md'), parse_mode='Markdown')
     return None
 
-def _display_football_feed():
+def _display_football_feed(chat_id):
     football_items = parse_football_sportru_rss()
     for news in football_items:
         fmsg = render_to_string('feed.md', news)
@@ -32,7 +32,7 @@ def _display_football_feed():
     return None
 
 
-def _display_hockey_feed():
+def _display_hockey_feed(chat_id):
     hockey_items = parse_hockey_sportru_rss()
     for news in hockey_items:
         hmsg = render_to_string('feed.md', news)
@@ -40,7 +40,7 @@ def _display_hockey_feed():
 
     return None
 
-def _start_payments():
+def _start_payments(chat_id):
     return render_to_string('feed.md', {'items': parse_hockey_sportru_rss()})
 
 class CommandReceiveView(View):
