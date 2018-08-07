59cabec84bc6449272ba78311fe5bc8a2da90916
perevozchikov <41132287+perevozchikov@users.noreply.github.com>
2018-08-07 18:22:16 +1000

    Add files via upload

--- SportInformBot/views.py
+++ SportInformBot/views.py
@@ -78,8 +78,8 @@ class CommandReceiveView(View):
                 query_id, from_id, query_data = telepot.glance(payload['message'], flavor='callback_query')
 
                 cmd = query_data
-                chat_id = from_id
-                
+                chat_id = query_id
+
             elif flavor == 'chat':
                 chat_id = payload['message']['chat']['id']
                 cmd = payload['message'].get('text')  # command
