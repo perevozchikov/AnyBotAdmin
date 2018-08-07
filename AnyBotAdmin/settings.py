d5fd31a3645e15ee5eaabed4972a49763a7a648a
perevozchikov <41132287+perevozchikov@users.noreply.github.com>
2018-08-08 09:32:22 +1000

    Add files via upload

--- AnyBotAdmin/settings.py
+++ AnyBotAdmin/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '!tzh82mc1$lvscphfkmn1r7v+^f1skri266$@j)fl3nwh+es%('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'perevozchikov.online']
 # DEBUG = env.bool('DEBUG', True)
