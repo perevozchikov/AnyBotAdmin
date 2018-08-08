aea9e0b548fe4e56f4961e374727e6fb3bb20b57
perevozchikov <41132287+perevozchikov@users.noreply.github.com>
2018-08-08 10:10:19 +1000

    Add files via upload

--- SportInformBot/views.py
+++ SportInformBot/views.py
@@ -72,19 +72,21 @@ class CommandReceiveView(View):
         else:
 
 
-            flavor = telepot.flavor(payload['message'])
+            #flavor = telepot.flavor(payload['message'])
+            if telepot.flavor(payload['callback_query']):
+                flavor = telepot.flavor(payload['callback_query'])
+                if flavor == 'callback_query':
 
-            if flavor == 'callback_query':
-
-                query_id, from_id, query_data = telepot.glance(payload['message'], flavor='callback_query')
-                TelegramBot.sendMessage(chat_id, query_data, parse_mode='Markdown')
-                cmd = query_data
-                chat_id = from_id
-
-            elif flavor == 'chat':
-                chat_id = payload['message']['chat']['id']
-                cmd = payload['message'].get('text')  # command
-                TelegramBot.sendMessage(chat_id, flavor, parse_mode='Markdown')
+                    query_id, from_id, query_data = telepot.glance(payload['callback_query'], flavor='callback_query')
+                    TelegramBot.sendMessage(chat_id, query_data, parse_mode='Markdown')
+                    cmd = query_data
+                    #chat_id = from_id
+            elif telepot.flavor(payload['message']):
+                flavor = telepot.flavor(payload['message'])
+                elif flavor == 'chat':
+                    chat_id = payload['message']['chat']['id']
+                    cmd = payload['message'].get('text')  # command
+                    TelegramBot.sendMessage(chat_id, flavor, parse_mode='Markdown')
 
 
 
