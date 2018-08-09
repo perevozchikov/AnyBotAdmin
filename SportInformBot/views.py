ccf7a0ab6868a47a8f71b2ee792a86fdbf153e5e
perevozchikov <41132287+perevozchikov@users.noreply.github.com>
2018-08-09 22:23:17 +1000

    Add files via upload

--- SportInformBot/views.py
+++ SportInformBot/views.py
@@ -73,7 +73,7 @@ def _payment_succes(chat_id):
 def _display_video(chat_id):
     linkvideo = 'https://youtu.be/WLW4SYJve4c'
     TelegramBot.sendMessage(chat_id, linkvideo, parse_mode='Markdown')
-    linkvid = 'https://youtu.be/2bD4oUBoJGM'
+    linkvid = str('@vid https://youtu.be/2bD4oUBoJGM')
     TelegramBot.sendMessage(chat_id, linkvid, parse_mode='Markdown')
 
     return None
