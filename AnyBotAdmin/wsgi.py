bdda0e2ea84e74c7b81e6137c681629831293c77
perevozchikov <41132287+perevozchikov@users.noreply.github.com>
2018-08-09 14:40:39 +1000

    Add files via upload

--- AnyBotAdmin/wsgi.py
+++ AnyBotAdmin/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "AnyBotAdmin.settings")
 
-application = get_wsgi_application()
+from django.core.wsgi import get_wsgi_application
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+application = StaticFilesHandler(get_wsgi_application())
+#application = get_wsgi_application()
