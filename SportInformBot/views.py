407b02db097462425e50e99c64d026bb2d94ceb0
perevozchikov <41132287+perevozchikov@users.noreply.github.com>
2018-08-09 15:13:02 +1000

    Add files via upload

--- SportInformBot/views.py
+++ SportInformBot/views.py
@@ -59,7 +59,7 @@ def _send_invoice(chat_id):
                 payload='a-string-identifying-related-payment-messages-tuvwxyz',
                 provider_token=settings.PAYMENT_PROVIDER_TOKEN,
                 start_parameter='abc',
-                currency='RUB', prices=[LabeledPrice(label='One Case', amount=1800000),], photo_url='http://perevozchikov.online/static/PS4-1024x576.jpg',
+                currency='RUB', prices=[LabeledPrice(label='One Case', amount=1800000),], photo_url='PS4-1024x576.jpg',
                 need_shipping_address=True, is_flexible=True)  # required for shipping query
     TelegramBot.sendInvoice(chat_id, "Сабвуфер Kixx ICQ250(USED)", "Активный сабвуфер",
                 payload='a-string-identifying-related-payment-messages-tuvwxyz',
